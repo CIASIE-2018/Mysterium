d7695e66ba2f5454b01052fdf888ea1416472edf
Johan SALTUTTI <jo.saltutti@orange.fr>
2018-09-25 18:11:07 +0200

    Initialisation du jeu

--- game.js
+++ game.js
@@ -1,7 +1,9 @@
 let uniqid = require('uniqid');
+let fs     = require('fs');
+let helpers = require('./helpers');
 
 class Game {
-    constructor(max_player = 7, max_turn = 7){
+    constructor(max_player = 7, max_turn = 7, difficulte = 0){
         if(max_turn < 4)
             throw new Error('Minimum 4 turn')
         this.id         = uniqid();
@@ -10,10 +12,14 @@ class Game {
         this.max_turn   = max_turn;
         this.turn       = 0;
         this.started    = false;
+        this.difficulte = 0;
+        this.persos     = [];
+        this.lieux      = [];
+        this.armes      = [];
     }
 
     join(playerId){
-        if(this.players.length + 1 <= this.max_player){
+        if(!this.isFull && !this.started){
             this.players.push({
                 id    : playerId,
                 ready : false
@@ -50,28 +56,47 @@ class Game {
 
     init(){
         if(this.allIsReady){
+
             this.started = true;
+            
+            let nb_scenarios = this.players.length - 1 + (this.difficulte == 0 ? 2 : (this.difficulte == 1 ? 3 : 0));
+            let persos = fs.readdirSync(__dirname + '/assets/images/personnage');
+            this.persos = helpers.shuffle(persos).slice(0, nb_scenarios);
+
+            let lieux = fs.readdirSync(__dirname + '/assets/images/lieux');
+            this.lieux = helpers.shuffle(lieux).slice(0, nb_scenarios);
+
+            let armes = fs.readdirSync(__dirname + '/assets/images/armes');
+            this.armes = helpers.shuffle(armes).slice(0, nb_scenarios);
+
+            let aleaScenarioFinal = Math.floor(Math.random() * (this.players.length - 1));
+            this.scenarioFinal = {
+                perso : this.persos[aleaScenarioFinal],
+                lieu  : this.lieux[aleaScenarioFinal],
+                arme  : this.armes[aleaScenarioFinal]
+            }
         }else{
             throw new Error('Tous les joueurs ne sont pas pret');
         }
-        //Générer les scénarios
-        //Choisir le scénario final
         //Attribuer un role a chaque joueur
         //Attribuer un role unique a chaque médium
     }
-
 }
 
 let g = new Game();
 
 let user1 = uniqid();
 let user2 = uniqid();
+let user3 = uniqid();
+
 
 g.join(user1);
 g.join(user2);
+g.join(user3);
 
 g.setState(user1, true);
 g.setState(user2, true);
+g.setState(user3, true);
 g.init();
 
 console.log(g);
