1c49b3901f4b95d2055d0a572c9ff2e5a2ba1478
Johan SALTUTTI <jo.saltutti@orange.fr>
2018-10-01 14:50:33 +0200

    Mise en place du moteur de template Twig

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 const config = require('./config/config');
 
-let express = require('express');
-let app     = express();
-let server  = require('http').createServer(app);
-let io      = require('socket.io').listen(server);
-let fs      = require('fs');
+let express      = require('express');
+let app          = express();
+let server       = require('http').createServer(app);
+let io           = require('socket.io').listen(server);
+
+app.set('views', __dirname + '/ressources/views');
+app.set("twig options", {
+    allow_async: true,
+    strict_variables: false
+});
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-    fs.readFile('./index.html', 'utf-8', function(error, content) {
-        res.writeHead(200, {"Content-Type": "text/html"});
-        res.end(content);
-    });
+    res.render('game.twig');
 });
 
 //Middleware when route not exist
@@ -20,7 +22,6 @@ app.use(function(req, res, next){
     res.status(404);
     res.type('txt').send('Not found');
 });
-
 
 server.listen(config.app.port, () => {
     console.log(`Server run on port ${config.app.port}`);
