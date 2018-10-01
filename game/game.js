a0277ff1a3c2b953a77acc317372c464cc33be18
Johan SALTUTTI <jo.saltutti@orange.fr>
2018-10-01 17:24:17 +0200

    Structure de base game + test avec immer

--- game/game.js
+++ game/game.js
@@ -1,3 +1,8 @@
-exports.join = (game, playerId) => {
-    
+let { produce } = require('immer'); /* https://github.com/mweststrate/immer */
+
+exports.join = (baseGame, playerId) => {
+    let newGame = produce(baseGame, draftGame => {
+        draftGame.players.push(playerId);
+    });
+    return newGame;
 }
