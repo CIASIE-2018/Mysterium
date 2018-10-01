6dfe03743a70698fc48c55cccf0263c964a7c64f
AnthonyNancy <anthony.zink@outlook.fr>
2018-10-01 19:14:29 +0200

    changement de la structure de Game class->object

--- game/game.js
+++ game/game.js
@@ -1,5 +1,36 @@
 let { produce } = require('immer'); /* https://github.com/mweststrate/immer */
 
+exports.game = {
+    id         : uniqid(),
+    max_player : 7,
+    max_turn   : 7,
+    turn       : 0,
+    started    : false,
+    difficulte : 0,
+    persos     : [],
+    lieux      : [],
+    armes      : [],
+    scenario_final: {},
+    players    : [
+        //Structure des joueurs
+        {
+            role            : 'ghost',
+            id              : null,
+            ready           : false,
+            hand            : [],
+            mediumsHasCards : []
+        },
+        {
+            role      : 'medium',
+            id        : null,
+            ready     : false,
+            state     : 0,
+            visions   : [],
+            hasPlayed : false
+        }
+    ],
+}
+
 exports.join = (baseGame, playerId) => {
     let newGame = produce(baseGame, draftGame => {
         draftGame.players.push(playerId);
