8888661f57d94300d57f9f860b90812cc71767b5
Johan SALTUTTI <jo.saltutti@orange.fr>
2018-10-03 13:38:56 +0200

    Ajout de la fonction generate_cards + fix init_roles (game.js)

--- game/game.js
+++ game/game.js
@@ -1,6 +1,10 @@
 const { produce } = require('immer'); /* https://github.com/mweststrate/immer */
 const errors      = require('./Error.js');
 
+let fs       = require('fs');
+let helpers  = require('../helpers');
+const config = require('../config/config');
+
 exports.join = (baseGame, playerId) => {
     let player = baseGame.players.find(player => player.id === playerId);
     if(player != undefined)
@@ -33,20 +37,73 @@ exports.init_roles = baseGame => {
 
     let aleaGhost = Math.floor(Math.random() * baseGame.players.length);
 
-    let players   = baseGame.players.map((player, i) => {
-        if(aleaGhost === i){
-            player.role            = 'ghost';
-            player.hand            = [];
-            player.mediumsHasCards = [];
-        }else{
-            player.role            = 'medium';
-            player.state           = 0;
-            player.visions         = [];
-            player.hasPlayed       = false;
-        }
-    });
-
     return produce(baseGame, draftGame => {
-        draftGame.players = players;
+        draftGame.ghost   = null;
+        draftGame.mediums = [];
+
+        draftGame.players.forEach((player, i) => {
+            if(aleaGhost === i){
+                draftGame.ghost = {
+                    id              : player.id,
+                    hand            : [],
+                    mediumsHasCards : []
+                };
+            }else{
+                draftGame.mediums.push({
+                    id        : player.id,
+                    state     : 0,
+                    visions   : [],
+                    hasPlayed : false
+                });
+            }
+        });
+        delete draftGame.players;
     });
 }
+
+exports.generate_cards = baseGame => {
+    let nb_scenarios = baseGame.mediums.length;
+    switch(baseGame.difficulte){
+        case 0 : 
+            nb_scenarios+=2;
+        break;
+        case 1 :
+            nb_scenarios+=3;
+        break;
+    }
+    return produce(baseGame, draftGame => {
+        draftGame.persos = getRandomFiles(config.directory.images + '/personnage', nb_scenarios);
+        draftGame.lieux  = getRandomFiles(config.directory.images + '/lieux'     , nb_scenarios);
+        draftGame.armes  = getRandomFiles(config.directory.images + '/armes'     , nb_scenarios);
+    });
+}
+
+function getRandomFiles(path, nb_files){
+    let files  = fs.readdirSync(path);
+    files      = helpers.shuffle(files).slice(0, nb_files);
+    return files;
+}
+
+
+let a = require('./game.js');
+
+let game = {
+    id         : 1233,
+    max_player : 7,
+    max_turn   : 7,
+    turn       : 0,
+    started    : false,
+    difficulte : 0,
+    persos     : [],
+    lieux      : [],
+    armes      : [],
+    players    : []
+}
+
+game = a.join(game, 'test1');
+game = a.join(game, 'test2');
+game = a.setReady(game, 'test1', true);
+game = a.setReady(game, 'test2', true);
+game = a.init_roles(game);
+game = a.generate_cards(game);
+console.log(game);
