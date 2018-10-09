81a9b617e74316822ec6bec6bf08805c81bf899b
JesterHeads <benjamin.thouvenin.54220@gmail.com>
2018-10-09 08:49:52 +0200

    creation du script chat pour le client

--- game/chat.js
+++ game/chat.js
@@ -1,1 +1,23 @@
-let socket = io.connect();
+let socket = io();//charge le module sur le client = connection au socket
+
+let pseudo = prompt('Quel est votre pseudo pour cette session ?')
+socket.emit('nouvel utilisateur', pseudo);
+
+$("#msg_form").submit(function (event) {
+    event.preventDefault();
+    socket.emit("chat message",{// transmission du message avec son contenu et son auteur
+        msg_content : $("#msg_writing").val(),
+        author : pseudo,
+    })
+    $("#msg_writing").val('').focus();
+    return false;
+});
+
+socket.on('chat message', (msg) => {
+    $('#msg_box').append($('<li>').html(msg));
+});
+
+//message a afficher a la connexion d'un joueur
+socket.on ('bienvenue', (msg) => {
+    $('#msg_box').append($('<li>').html(msg));
+});
