ffa6c285fd44d45041dd0880aa189ea8fe600adc
JesterHeads <benjamin.thouvenin.54220@gmail.com>
2018-10-15 21:54:30 +0200

    modif css

---
+++ game/client_chat.js
@@ -0,0 +1,22 @@
+$(function(){
+    let socket = io('/chat');//charge le module sur le client = connection au socket
+
+    $("#msg_form").submit(function (event) {
+        event.preventDefault();
+        socket.emit("chat message",{// transmission du message avec son contenu et son auteur
+            msg_content : $("#msg_writing").val(),
+            author : pseudo,
+        })
+        $("#msg_writing").val('').focus();
+        return false;
+    });
+
+    socket.on('chat message', (msg) => {
+        $('#msg_box').append($('<li>').html(msg));
+    });
+
+    //message a afficher a la connexion d'un joueur
+    socket.on ('bienvenue', (msg) => {
+        $('#msg_box').append($('<li>').html(msg));
+    });
+});
